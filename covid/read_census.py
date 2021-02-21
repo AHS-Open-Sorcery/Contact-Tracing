317d6e1c55c13b41222b726fcd155c665309b8b6
jadecodes8191 <jadecodes8191@gmail.com>
2021-02-21 16:02:51 -0500

    testing census encoding

---
+++ covid/read_census.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+pop_csv = pd.read_csv('https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv', encoding='latin1')
+
