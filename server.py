47e852b12d61bdf8363eed71bcbec703bafb293e
Daniel Ivanovich <dan@ivanovi.ch>
2021-02-21 16:59:12 -0500

    Add event bindings to server.py

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
+import uvicorn
 from blacksheep.server import Application
+from blacksheep.server.bindings import FromForm
 from blacksheep.server.templating import use_templates
 from jinja2 import PackageLoader
-import uvicorn
 
 app = Application()
 view = use_templates(app, loader=PackageLoader('app', 'templates'))
+
 
 @app.route('/')
 def home():
@@ -26,8 +28,36 @@ def about():
     return view('about', {})
 
 
-app.serve_files('static')
+@app.route('/join-event')
+def join_event():
+    return view('join-event', {})
 
+
+class EventCreationInput:
+    event_name: str
+    event_date: str
+    event_location: str
+    event_organizer: str
+    event_population: int
+    event_seats: int
+
+    def __init__(self, event_name: str, event_date: str, event_location: str, event_organizer: str,
+                 event_population: int, event_seats: int) -> None:
+        self.event_name = event_name
+        self.event_date = event_date
+        self.event_location = event_location
+        self.event_organizer = event_organizer
+        self.event_population = event_population
+        self.event_seats = event_seats
+
+
+@app.router.post('/event-creation-endpoint')
+async def create_event_from_post(input: FromForm[EventCreationInput]):
+    data = input.value
+
+
+
+app.serve_files('static')
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=42069)
